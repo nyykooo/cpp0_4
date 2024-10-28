50fae74699c0917130242ee9db709d171c653bb9
nyykooo <nicholas.cakla@outlook.com>
2024-10-28 16:13:09 +0000

    cpp00: modulating ex01 main function

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 13:50:58 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/10/25 16:47:07 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/10/28 16:10:29 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,8 @@ int	main(void)
 	while (1)
 	{
 		std::cout << "Please enter a comand (ADD, SEARCH, EXIT):" << std::endl;
-		if (!std::getline(std::cin, input))
+		if (!std::getline(std::cin, input) || input.compare("EXIT") == 0) // If EOF or EXIT, exit the program
 			break; 
-
-		if (input.compare("EXIT") == 0)
-		{
-			break;
-		}
 		else if (input.compare("ADD") == 0)
 			phone_book.ft_add_contact();
 		else if (input.compare("SEARCH") == 0)
