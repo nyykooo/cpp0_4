732b3f6e215f462564c978c47a09eed19bce0bca
nyykooo <nicholas.cakla@outlook.com>
2024-11-05 18:06:31 +0000

    better debugin logs

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 19:26:12 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/09/26 21:02:19 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:44:47 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,24 +24,28 @@ Harl::~Harl()
 
 void	Harl::debug( void )
 {
+	std::cout << std::endl << "[DEBUG]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-" <<
 		"ketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info( void )
 {
+	std::cout << std::endl << "[INFO]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough " <<
 		"bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning( void )
 {
+	std::cout << std::endl << "[WARNING]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years " <<
 		"whereas you started working here since last month." << std::endl;
 }
 
 void	Harl::error ( void )
 {
+	std::cout << std::endl << "[ERROR]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
