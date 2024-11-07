8bcd2b97fdbfb17623da41a3984173a6f1b9323c
Nicholas Campbell Caklamanis <ncampbel@c1r6s6.42lisboa.com>
2024-11-07 15:28:27 +0000

    fixing alloc of negative size protection, find empty string and improving harl debug

--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 11:08:50 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/09/25 11:51:20 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/07 15:19:18 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ Zombie *zombieHorde( int N, std::string name)
 {
 	int	i = 0;
 
+	if (N < 0)
+		return NULL;
 	Zombie *zombies = new Zombie[N];
 	while (i < N)
 	{
