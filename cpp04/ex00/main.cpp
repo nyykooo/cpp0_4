34eeaafdd9489a358cabe3d7da975efcfbc2f7b2
Nicholas Campbell Caklamanis <ncampbel@c2r6s6.42lisboa.com>
2024-11-08 17:48:15 +0000

    cpp04/ex00 e cpp02/ex01, improving main functions

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,16 +6,23 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:36:17 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/11/07 17:55:30 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/08 17:47:00 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
 
-int main(void)
+int main()
 {
-	Animal	animal;
-	Dog		dog;
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
 
-	return (0);
+	return 0;
 }
