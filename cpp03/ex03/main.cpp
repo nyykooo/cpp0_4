3f3d9d25f9f4ca4748945b0b0ba7b39cecf7b5a1
nyykooo <nicholas.cakla@outlook.com>
2024-11-19 20:46:04 +0000

    adding tests for getters

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ncampbel <ncampbel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 18:51:46 by ncampbel          #+#    #+#             */
-/*   Updated: 2024/11/19 20:28:06 by ncampbel         ###   ########.fr       */
+/*   Updated: 2024/11/19 20:43:20 by ncampbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,12 +76,38 @@ int main()
 	std::cout << "TESTING DIAMOND" << std::endl;
 	{
 		std::cout << "### CONSTRUCTORS ###" << std::endl;
-		DiamondTrap a;
-		DiamondTrap b("Jubileu");
+		ClapTrap clap;
+		ScavTrap scav;
+		FragTrap frag;
+		DiamondTrap diam("Diam");
+
+		
+		std::cout << std::endl << "### TESTING GETTERS ###" << std::endl;
+		std::cout << "## NAME ##" << std::endl;
+		std::cout << "ClapTrap name: " << clap.getName() << std::endl;
+		std::cout << "ScavTrap name: " << scav.getName() << std::endl;
+		std::cout << "FragTrap name: " << frag.getName() << std::endl;
+		std::cout << "DiamondTrap name: " << diam.getName() << std::endl;
+		std::cout << "## HP ##" << std::endl;
+		std::cout << "ClapTrap HP: " << clap.getHP() << std::endl;
+		std::cout << "ScavTrap HP: " << scav.getHP() << std::endl;
+		std::cout << "FragTrap HP: " << frag.getHP() << std::endl;
+		std::cout << "DiamondTrap HP: " << diam.getHP() << std::endl;
+		std::cout << "## EP ##" << std::endl;
+		std::cout << "ClapTrap EP: " << clap.getEP() << std::endl;
+		std::cout << "ScavTrap EP: " << scav.getEP() << std::endl;
+		std::cout << "FragTrap EP: " << diam.getEP() << std::endl;
+		std::cout << "DiamondTrap EP: " << diam.getEP() << std::endl;
+		std::cout << "## ATTACK ##" << std::endl;
+		std::cout << "ClapTrap Attack: " << clap.getAttack() << std::endl;
+		std::cout << "ScavTrap Attack: " << scav.getAttack() << std::endl;
+		std::cout << "FragTrap Attack: " << frag.getAttack() << std::endl;
+		std::cout << "DiamondTrap Attack: " << diam.getAttack() << std::endl;
+		
 
 		std::cout << std::endl << "### TESTING ACTIONS ###" << std::endl;
-		b.attack("some other robot");
-		b.whoAmI();
+		diam.attack("some other robot");
+		diam.whoAmI();
 	}
 	return (0);
 }
